03615853ff8df66647f1f9c00d6e15124476943e
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2017-02-17 22:40:12 -0500

    Add new game view and games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,20 @@
+class GamesController < ApplicationController
+  def new
+    @game = Game.new
+  end
+  
+  def create
+    @game = Game.new(game_params)
+    
+    if @game.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  def game_params
+    params.require(:game).permit(:name)
+  end
+end
