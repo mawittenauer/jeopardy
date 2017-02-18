7b99e0d5d2211e51311826b17b378c36d4cf607a
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2017-02-18 16:31:58 -0500

    Hide facebook secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1111103459019265', '825b984a028215f330b30adfd5107526'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
