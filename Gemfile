30b7eee2cb1b2dc9fd43983580c19c32bb16efef
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2017-02-18 15:12:35 -0500

    Add omniauth to user model and helper method for current user in application controller

--- Gemfile
+++ Gemfile
@@ -36,4 +36,3 @@ group :production do
   gem 'pg'
   gem 'rails_12factor'
 end
-
---
+++ spec/fabricators/user_fabricator.rb
@@ -0,0 +1,7 @@
+Fabricator(:user) do
+  provider         "MyString"
+  uid              "MyString"
+  name             "MyString"
+  oauth_token      "MyString"
+  oauth_expires_at "2017-02-18 15:08:01"
+end
