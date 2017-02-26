a46e796dde0b4e8702ee77a56a3b3218d4213626
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2017-02-26 15:27:19 -0500

    Add complete? method to games model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,10 @@ class Game < ActiveRecord::Base
   validates_presence_of :name
   has_many :categories
   belongs_to :user
+  
+  def complete?
+    sum = 0
+    categories.each { |category| sum += category.answers.count }
+    sum == 30
+  end
 end
